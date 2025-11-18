b4a03b957a659fe83ab7f71a0fbd1c4e1b91d1a1
Priyanshu Sil <122773433+rogueflux@users.noreply.github.com>
2025-11-18 22:31:09 +0530

    Add files via upload

---
+++ STM32 File/X-CUBE-AI/App/app_x-cube-ai.c
@@ -0,0 +1,69 @@
+
+/**
+  ******************************************************************************
+  * @file    app_x-cube-ai.c
+  * @author  X-CUBE-AI C code generator
+  * @brief   AI program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+
+ /*
+  * Description
+  *   v1.0 - Minimum template to show how to use the Embedded Client API
+  *          model. Only one input and one output is supported. All
+  *          memory resources are allocated statically (AI_NETWORK_XX, defines
+  *          are used).
+  *          Re-target of the printf function is out-of-scope.
+  *   v2.0 - add multiple IO and/or multiple heap support
+  *
+  *   For more information, see the embeded documentation:
+  *
+  *       [1] %X_CUBE_AI_DIR%/Documentation/index.html
+  *
+  *   X_CUBE_AI_DIR indicates the location where the X-CUBE-AI pack is installed
+  *   typical : C:\Users\[user_name]\STM32Cube\Repository\STMicroelectronics\X-CUBE-AI\7.1.0
+  */
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+
+#if defined ( __ICCARM__ )
+#elif defined ( __CC_ARM ) || ( __GNUC__ )
+#endif
+
+/* System headers */
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <inttypes.h>
+#include <string.h>
+
+#include "app_x-cube-ai.h"
+
+/* Entry points --------------------------------------------------------------*/
+
+void MX_X_CUBE_AI_Init(void)
+{
+}
+
+void MX_X_CUBE_AI_Process(void)
+{
+    /* USER CODE BEGIN 6 */
+    /* USER CODE END 6 */
+}
+#ifdef __cplusplus
+}
+#endif
